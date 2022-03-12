62096b2c43c377f484d1c2b524833163d2f26290
Yoshihiro Tanaka <contact@cordea.jp>
2022-03-12 17:55:57 +0900

    update text

--- Olakino/MainPage.xaml.cs
+++ Olakino/MainPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Globalization;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +14,34 @@ namespace Olakino
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void OnAddClick(object sender, RoutedEventArgs e)
+        {
+            var gram = GramTextBox.Text;
+            var amount = AmountTextBox.Text;
+            var percent = PercentTextBox.Text;
+        }
+
+        private void OnAmountTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateGram();
+        }
+
+        private void OnPercentTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateGram();
+        }
+
+        private void UpdateGram()
+        {
+            if (!int.TryParse(AmountTextBox.Text, out var amount) ||
+                !double.TryParse(PercentTextBox.Text, out var percent))
+            {
+                return;
+            }
+
+            GramTextBox.Text = (amount * percent).ToString(CultureInfo.CurrentCulture);
         }
     }
 }
